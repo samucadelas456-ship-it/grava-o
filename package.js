cc0642fc0bf23a8ea8f97f42539654d9f290a665
samucadelas456-ship-it <samucadelas456@gmail.com>
2026-03-19 22:57:26 -0300

    Update package.js

--- package.js
+++ package.js
@@ -5,21 +5,27 @@
   "main": "index.js",
   "scripts": {
     "start": "node index.js",
-    "dev": "nodemon index.js"
+    "dev": "nodemon index.js",
+    "build": "npm install",
+    "postinstall": "npm install ffmpeg-static@latest"
+  },
+  "engines": {
+    "node": ">=18.0.0",
+    "npm": ">=8.0.0"
   },
   "dependencies": {
     "discord.js": "^14.14.1",
     "@discordjs/voice": "^0.16.1",
     "ffmpeg-static": "^5.2.0",
-    "ffmpeg": "^0.0.4",
     "fluent-ffmpeg": "^2.1.2",
     "prism-media": "^1.3.5",
     "dotenv": "^16.3.1",
     "opusscript": "^0.0.8",
     "sodium": "^3.0.2",
     "libsodium-wrappers": "^0.7.13",
-    "fs": "^0.0.1-security",
-    "path": "^0.12.7",
     "moment": "^2.29.4"
+  },
+  "devDependencies": {
+    "nodemon": "^3.0.2"
   }
 }
