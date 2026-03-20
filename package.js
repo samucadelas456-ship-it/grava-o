718b462b3b3a2c5959feeb68e1fd5574afcae932
samucadelas456-ship-it <samucadelas456@gmail.com>
2026-03-19 22:44:59 -0300

    Create package.js

---
+++ package.js
@@ -0,0 +1,25 @@
+{
+  "name": "discord-recording-bot",
+  "version": "1.0.0",
+  "description": "Bot para gravação de calls do Discord",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js",
+    "dev": "nodemon index.js"
+  },
+  "dependencies": {
+    "discord.js": "^14.14.1",
+    "@discordjs/voice": "^0.16.1",
+    "ffmpeg-static": "^5.2.0",
+    "ffmpeg": "^0.0.4",
+    "fluent-ffmpeg": "^2.1.2",
+    "prism-media": "^1.3.5",
+    "dotenv": "^16.3.1",
+    "opusscript": "^0.0.8",
+    "sodium": "^3.0.2",
+    "libsodium-wrappers": "^0.7.13",
+    "fs": "^0.0.1-security",
+    "path": "^0.12.7",
+    "moment": "^2.29.4"
+  }
+}
